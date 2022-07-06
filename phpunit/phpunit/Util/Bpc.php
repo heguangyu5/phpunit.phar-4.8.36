402890d6564fda679df28b2f84bf0e3342ff2cad
heguangyu5 <heguangyu5@qq.com>
2022-07-06 04:36:30 +0000

    当以 phpunit path/to/TestSuiteFile.php 的方式多次运行时,要将TestSuiteFile.php累积起来

--- phpunit/phpunit/Util/Bpc.php
+++ phpunit/phpunit/Util/Bpc.php
@@ -2,6 +2,7 @@
 
 class PHPUnit_Util_Bpc
 {
+    // className => filename
     protected static $testSuiteClasses = array();
 
     public static function collectTestSuiteClass($className, $filename)
@@ -11,17 +12,34 @@ class PHPUnit_Util_Bpc
 
     public static function generateEntryFile()
     {
-        // className => filename
-        $prefixLen = strlen(getcwd()) + 1;
+        $cwd = getcwd();
+        $mapFile = $cwd . '/TESTCASE_LIST.php';
+        if (file_exists($mapFile)) {
+            $map = include $mapFile;
+            $mapFileIncluded = true;
+        } else {
+            $map = array();
+            $mapFileIncluded = false;
+        }
+
+        $prefixLen = strlen($cwd) + 1;
         foreach (self::$testSuiteClasses as $className => $filename) {
-            self::$testSuiteClasses[$className] = substr($filename, $prefixLen);
+            $map[$className] = substr($filename, $prefixLen);
         }
 
         file_put_contents(
-            'run-test.php',
+            $mapFile,
+            '<?php return ' . var_export($map, true) . ';'
+        );
+        if (!$mapFileIncluded) {
+            include $mapFile;
+        }
+
+        file_put_contents(
+            $cwd . '/run-test.php',
             "<?php
 define('RUN_ROOT_DIR', __DIR__);
-define('TESTCASE_LIST', " . var_export(self::$testSuiteClasses, true) . ");
+define('TESTCASE_LIST', include __DIR__ . '/TESTCASE_LIST.php');
 
 include 'phpunit/loader.php';
 PHPUnit_TextUI_Command::main();
@@ -54,7 +72,7 @@ PHPUnit_TextUI_Command::main();
 
     public static function saveMakefile()
     {
-        if (in_array('PHPUnit_DbUnit_TestCase', get_declared_classes())) {
+        if (class_exists('PHPUnit_DbUnit_TestCase', false)) {
             $phpunitExt = '-u phpunit-ext ';
         } else {
             $phpunitExt = '';
@@ -73,7 +91,7 @@ test: $(FILES)
 
 clean:
 	@rm -rf .bpc-build-* md5.map
-	@rm -fv $(FILES) test
+	@rm -fv $(FILES) TESTCASE_LIST.php test
 	@rm -rf MockClassFile
 MAKEFILECODR;
 
